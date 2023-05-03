b63d20353be7db709a3eaa18a69da6faf976e62e
José Paulo <jose.silva@bandtec.com.br>
2023-05-03 16:53:24 -0300

    alterando script lambda

--- script/lambda/google-cloud-functions/main.py
+++ script/lambda/google-cloud-functions/main.py
@@ -1,8 +1,19 @@
 import functions_framework
+import json
+import logging
 
 @functions_framework.http
 def hello_http(request):    
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    try:
+        request_args = request.get_json()
 
-    return '{}'.format(request_json)
+        if not request_args:
+            return {"error": "A solicitação não contém um corpo."}
+
+        response_json = json.dumps(request_args)
+
+        return response_json, 200, {'Content-Type': 'application/json'}
+
+    except Exception as e:
+        logging.exception(e)
+        raise

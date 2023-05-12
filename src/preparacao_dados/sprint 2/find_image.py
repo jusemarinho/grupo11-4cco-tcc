9f95ea6f183baacb006e860c6107d57a6ee4862a
José Paulo <jose.silva@bandtec.com.br>
2023-05-11 22:36:44 -0300

    add: script para coletar imagens

---
+++ src/preparacao_dados/sprint 2/find_image.py
@@ -0,0 +1,64 @@
+import boto3
+import json
+from dotenv import load_dotenv
+import os
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+
+load_dotenv()
+
+
+class FindImage:
+    def __init__(
+        self, flag: bool = None, bucket_name: str = None, resource_name: str = None
+    ) -> None:
+        self.flag = flag
+        if self.flag:
+            self.session = self.create_session_boto3()
+            self.bucket_name = bucket_name
+            self.resource = self.create_resource(resource_name, self.session)
+            self.s3_bucket = self.s3(bucket_name, self.resource)
+
+    def create_session_boto3(self):
+        session = boto3.Session(
+            aws_access_key_id=os.getenv("aws_access_key_id"),
+            aws_secret_access_key=os.getenv("aws_secret_access_key"),
+            aws_session_token=os.getenv("aws_session_token"),
+            region_name=os.getenv("AWS_DEFAULT_REGION"),
+        )
+
+        return session
+
+    def create_resource(self, resource: str, session: boto3.Session):
+        return session.resource(resource)
+
+    def s3(self, bucket_name, resource: str):
+        return resource.Bucket(bucket_name)
+
+    def find_image_by_id_user_id_name_pet(
+        self, md5: str, user_id: str = None, id_pet: str = None, name_pet: str = None
+    ):
+        if self.flag:
+            image_key = f"train/{user_id}/{id_pet}/{name_pet}_{md5}.jpg"
+            print(image_key)
+            obj = self.s3_bucket.Object(image_key)
+            image_data = obj.get()["Body"].read()
+            image = Image.open(BytesIO(image_data))
+        else:
+            # procurar fotos localmente
+            local_image_path = f"../../inteligencia_artificial/train/{md5}.jpg"
+            if os.path.exists(local_image_path):
+                image = Image.open(local_image_path)
+            else:
+                print("A imagem não foi encontrada localmente.")
+                return
+
+        plt.imshow(image)
+        plt.axis("off")
+        plt.show()
+
+
+if __name__ == "__main__":
+    find_image = FindImage(flag=False)
+    find_image.find_image_by_id_user_id_name_pet('50fcbd042ae9a24e3afd5cbb8e1e4542')

45165202d51bf8f60415b96d3f6733cc938e5bda
José Paulo <jose.silva@bandtec.com.br>
2023-10-13 21:26:20 -0300

    add: script para fazer o download de imagens do instagram

--- script/download-image-instagram/main.py
+++ script/download-image-instagram/main.py
@@ -1,29 +1,71 @@
-from requests import get
-from json import loads
+from argparse import ArgumentParser
+from glob import glob
+from os.path import expanduser
+from platform import system
+from sqlite3 import OperationalError, connect
+from instaloader import Instaloader, Profile
 
-url = 'https://instagram.com/iamjuse'
-
-params = { '__a': 1, '__d': 1 }
-
-cookies = { 'sessionid': '635954753%3AMR2e92kELPo7KN%3A28%3AAYe7El72Yax2XD-K1ailWbaWDkDB3l2JvRkZ6kZlZw' }
-
-def on_success(response):
-    profile_data_json = response.text
-    parsed_data = loads(profile_data_json)
-    print(parsed_data)
-    
-    # print('User fullname:', parsed_data['graphql']['user']['full_name'])
-    # print('User bio:', parsed_data['graphql']['user']['biography'])
-
-def on_error(response):
-    print('Something went wrong')
-    print('Error Code:', response.status_code)
-    print('Reason:', response.reason)
+try:
+    from instaloader import ConnectionException, Instaloader
+except ModuleNotFoundError:
+    raise SystemExit("Instaloader not found.\n  pip install [--user] instaloader")
 
 
-response = get(url, params, cookies=cookies)
+def get_cookiefile():
+    default_cookiefile = {
+        "Windows": "~/AppData/Roaming/Mozilla/Firefox/Profiles/*/cookies.sqlite",
+        "Darwin": "~/Library/Application Support/Firefox/Profiles/*/cookies.sqlite",
+    }.get(system(), "~/.mozilla/firefox/*/cookies.sqlite")
+    cookiefiles = glob(expanduser(default_cookiefile))
+    if not cookiefiles:
+        raise SystemExit("No Firefox cookies.sqlite file found. Use -c COOKIEFILE.")
+    return cookiefiles[0]
 
-if response.status_code == 200:
-    on_success(response)
-else:
-    on_error(response)
+
+def import_session(cookiefile, sessionfile):
+    print("Using cookies from {}.".format(cookiefile))
+    conn = connect(f"file:{cookiefile}?immutable=1", uri=True)
+    try:
+        cookie_data = conn.execute(
+            "SELECT name, value FROM moz_cookies WHERE baseDomain='instagram.com'"
+        )
+    except OperationalError:
+        cookie_data = conn.execute(
+            "SELECT name, value FROM moz_cookies WHERE host LIKE '%instagram.com'"
+        )
+    instaloader = Instaloader(max_connection_attempts=1)
+    instaloader.context._session.cookies.update(cookie_data)
+    username = instaloader.test_login()
+    if not username:
+        raise SystemExit("Not logged in. Are you logged in successfully in Firefox?")
+    print("Imported session cookie for {}.".format(username))
+    instaloader.context.username = username
+    instaloader.save_session_to_file(sessionfile)
+
+def download_images(username, num_images):
+    loader = Instaloader()
+    profile = Profile.from_username(loader.context, username)
+
+    for post in profile.get_posts():
+        loader.download_post(post, target=profile.username)
+        num_images -= 1
+        if num_images == 0:
+            break
+
+
+if __name__ == "__main__":
+    p = ArgumentParser()
+    p.add_argument("-c", "--cookiefile")
+    p.add_argument("-f", "--sessionfile")
+    args = p.parse_args()
+
+    username_to_download = 'neymarjr'
+
+    number_of_images_to_download = 10
+
+    download_images(username_to_download, number_of_images_to_download)
+
+    try:
+        import_session(args.cookiefile or get_cookiefile(), args.sessionfile)
+    except (ConnectionException, OperationalError) as e:
+        raise SystemExit("Cookie import failed: {}".format(e))

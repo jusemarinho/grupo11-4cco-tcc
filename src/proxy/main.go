7152269d8d78350a35c316d9ed4116533d4f7fdb
José Paulo <jose.silva@bandtec.com.br>
2023-11-23 22:17:20 -0300

    chore(): atualizando script para aceitar conexão com servidor websocket

--- src/proxy/main.go
+++ src/proxy/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+
+	"github.com/gorilla/websocket"
 )
 
 func main() {
@@ -17,14 +19,64 @@ func main() {
 		return
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+	targetURL := &url.URL{
 		Scheme: "http",
 		Host:   targetHost + ":" + targetPort,
-	})
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Proxying request: %s %s\n", r.Method, r.URL.Path)
 		proxy.ServeHTTP(w, r)
+	})
+
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println("Erro ao atualizar a conexão WebSocket:", err)
+			return
+		}
+		defer conn.Close()
+
+		targetConn, _, err := websocket.DefaultDialer.Dial(targetURL.String(), nil)
+		if err != nil {
+			fmt.Println("Erro ao conectar ao destino via WebSocket:", err)
+			return
+		}
+		defer targetConn.Close()
+
+		go func() {
+			for {
+				messageType, p, err := conn.ReadMessage()
+				if err != nil {
+					fmt.Println("Erro ao ler mensagem do cliente:", err)
+					return
+				}
+				if err := targetConn.WriteMessage(messageType, p); err != nil {
+					fmt.Println("Erro ao escrever mensagem para o destino:", err)
+					return
+				}
+			}
+		}()
+
+		for {
+			messageType, p, err := targetConn.ReadMessage()
+			if err != nil {
+				fmt.Println("Erro ao ler mensagem do destino:", err)
+				return
+			}
+			if err := conn.WriteMessage(messageType, p); err != nil {
+				fmt.Println("Erro ao escrever mensagem para o cliente:", err)
+				return
+			}
+		}
 	})
 
 	proxyPort := "8080"

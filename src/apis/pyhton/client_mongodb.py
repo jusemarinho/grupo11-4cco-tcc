11e9da26e2c67210c56def06c68c5bedc5802bfe
José Paulo <jose.silva@bandtec.com.br>
2023-11-26 15:47:38 -0300

    update(): inputando senha e usuário no mongodb

--- src/apis/pyhton/client_mongodb.py
+++ src/apis/pyhton/client_mongodb.py
@@ -1,9 +1,17 @@
 import pymongo
+from urllib.parse import quote_plus
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class ClientMongoDb:
 
     def update_result_recognator(self, endToEnd: str, result: list):
-        client = pymongo.MongoClient("mongodb://localhost:27017")  
+        username = quote_plus(os.getenv("MONGODB_USERNAME"))
+        password = quote_plus(os.getenv("MONGODB_PASSWORD"))
+
+        client = pymongo.MongoClient(f"mongodb://{username}:{password}@localhost:27017/ACHEI_O_BICHO")
 
         db = client["ACHEI_O_BICHO"]
         colecao = db["RECOGNIZE_PET"]
